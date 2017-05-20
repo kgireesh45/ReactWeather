5b68adb78cff59618680460ec1d57f8812492a32
Gireesh <Gireesh@gmail.com>
2017-05-20 10:59:34 -0500

    update

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 var express = require('express');
 
-var app= express();
+// Create our app
+var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function(req,res,next){
-  if (req.headers['x-forward-proto']==='http'){
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
-    res.redirect('http://'+req.hostname+req.url);
+    res.redirect('http://' + req.hostname + req.url);
   }
 });
+
 app.use(express.static('public'));
 
-app.listen(PORT, function(){
-  console.log("express server is runinng on port "+ PORT);
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
 });
