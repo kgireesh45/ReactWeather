64e59bb7ccacbb3bc8663d4dcdff2606001107f7
Gireesh <Gireesh@gmail.com>
2017-05-20 11:11:05 -0500

    examples modification

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,7 @@ var React= require('react');
 var Examples = (props)=> {
   return(
     <div>
-      <p>Examples Component</p>
+      <p>Examples </p>
     <h3>This is from Examples Component!</h3>
     </div>
   )
